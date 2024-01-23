92f609b4f8432b7df0f289769d0073ad1be0319d
Atendimento2005 <gogo.june2005@gmail.com>
2024-01-23 10:40:16 +0530

    dev working

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-dayjs.extend(window.dayjs_plugin_timezone)
+dayjs.extend(window.dayjs_plugin_timezone);
+dayjs.extend(window.dayjs_plugin_utc);
 
-function updateTime(time){
-  time.innerText = dayjs().format("HH:mm:ss")
+function update(time, timezone, timezoneInput, date) {
+	time.innerText = dayjs().tz(timezoneInput.value).format("HH:mm:ss");
+	timezone.innerText = timezoneInput.value;
+	date.innerText = dayjs().tz(timezoneInput.value).format("dddd, D MMMM, YYYY");
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  setInterval(() => {
-    updateTime(document.body.querySelector("#time"))
-    document.body.querySelector('#timezone').innerText = dayjs.tz.guess()
-    document.body.querySelector('#date').innerText = dayjs().format("dddd, D MMMM, YYYY")
-  }, 1);
+	let timezoneInput = document.querySelector("#timezone-input");
 
-})
+	fetch("timezones.txt")
+		.then((res) => res.text())
+		.then((text) => {
+			let data = text.split("\n");
+			let tzOptions = "";
+			data.forEach((elem) => {
+				tzOptions += `<option value="${elem}">${elem}</option>\n`;
+			});
+			timezoneInput.innerHTML = tzOptions;
+			timezoneInput.value = dayjs.tz.guess();
+		});
+
+	setInterval(() => {
+		update(
+			document.body.querySelector("#time"),
+			document.body.querySelector("#timezone"),
+			document.body.querySelector("#timezone-input"),
+			document.body.querySelector("#date")
+		);
+	}, 100);
+
+	document
+		.querySelector("#timezone")
+		.setAttribute("data-micromodal-trigger", "tz-modal");
+
+	MicroModal.init();
+});
